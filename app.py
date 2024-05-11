bb9ece39ad3fd18199c70b461121276ffedd63b7
Muhammad Mohsin Rashid <mohsinrashid64@gmail.com>
2024-05-11 17:15:03 +0500

    streamlit-deployment-3rd-commit

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import os
 import streamlit as st
 from dotenv import load_dotenv
+
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 from langchain_community.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from langchain_openai import OpenAI, OpenAIEmbeddings
